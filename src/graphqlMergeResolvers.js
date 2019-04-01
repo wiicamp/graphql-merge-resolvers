7fd76b2a2f3a6dba8584ea3ed2ee95378076498c
Ha Vinh Thai <vinhthai997@gmail.com>
2019-04-02 01:55:53 +0700

    Fix eslint (done)

--- src/graphqlMergeResolvers.js
+++ src/graphqlMergeResolvers.js
@@ -6,12 +6,16 @@ class GraphqlMergeResolvers {
     if (Array.isArray(customResolver)) {
       customResolver.forEach((resolver) => {
         for (key in resolver) {
-          mergedResolver[key] = { ...mergedResolver[key], ...resolver[key] };
+          if ({}.hasOwnProperty.call(resolver, key)) {
+            mergedResolver[key] = { ...mergedResolver[key], ...resolver[key] };
+          }
         }
       });
     } else {
       for (key in customResolver) {
-        mergedResolver[key] = { ...mergedResolver[key], ...customResolver[key] };
+        if ({}.hasOwnProperty.call(customResolver, key)) {
+          mergedResolver[key] = { ...mergedResolver[key], ...customResolver[key] };
+        }
       }
     }
 
