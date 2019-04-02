6f2c62c4be431be80f63324c67d9be37a149535c
Ha Vinh Thai <vinhthai997@gmail.com>
2019-04-02 08:12:16 +0700

    Add typescript file (done)

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module "graphql-merge-resolvers" {
+  type CustomResolvers = any;
+  type RootResolver = object;
+
+  export default class GraphqlMergeResolvers { };
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import GraphqlMergeResolvers from './graphqlMergeResolvers';
+class GraphqlMergeResolvers {
+  static merge(customResolver, rootResolver = {}) {
+    const mergedResolver = rootResolver;
+    let key;
+
+    if (Array.isArray(customResolver)) {
+      customResolver.forEach((resolver) => {
+        for (key in resolver) {
+          if ({}.hasOwnProperty.call(resolver, key)) {
+            mergedResolver[key] = { ...mergedResolver[key], ...resolver[key] };
+          }
+        }
+      });
+    } else {
+      for (key in customResolver) {
+        if ({}.hasOwnProperty.call(customResolver, key)) {
+          mergedResolver[key] = { ...mergedResolver[key], ...customResolver[key] };
+        }
+      }
+    }
+
+    return mergedResolver;
+  }
+}
 
 export default GraphqlMergeResolvers;
