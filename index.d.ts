966e43ce07f78469fa11fb8c5fb171c2897916aa
Vinh Thai <vinhthai997@gmail.com>
2019-04-02 09:26:50 +0700

    Change type return of module (done)

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 declare module "graphql-merge-resolvers" {
   class GMR {
-    merge(customResolvers: any, rootResolver: object): any
+    merge(customResolvers: any, rootResolver: object): object
   }
 
   namespace GMR { }
